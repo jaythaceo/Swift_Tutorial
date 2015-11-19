d824b4135b4446c61e73b33e74bdf664587a4be3
jaythaceo <jaythaceo@gmail.com>
2015-11-19 17:10:07 -0500

    [chore] Added Employee commsion class

---
+++ Employee/CommissionEmployee.swift
@@ -0,0 +1,9 @@
+//
+//  CommissionEmployee.swift
+//  
+//
+//  Created by Jason  Brooks on 11/19/15.
+//
+//
+
+import Foundation
---
+++ swift1.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Variables
+var number = 34
+var decimal = 3.4
+
+// Constants
+let people = 1
+
+// Type Annotations
+var welcomeMessage = "Hello, how's it going?"
+
+// Printing constants and variables
+print(number)
+
+
+
